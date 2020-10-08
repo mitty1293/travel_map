5e4b2ceaa43dabfaf753378b54bd30ebc8f7825e
mitty1293 <k.fmichi@gmail.com>
2020-10-08 12:27:20 +0900

    db修正

--- db/initdb.d/create_tbl.sql
+++ db/initdb.d/create_tbl.sql
@@ -4,3 +4,14 @@ CREATE TABLE `travel_map_db`.`travel_map_tbl`(
     PRIMARY KEY (`id`),
     SPATIAL KEY (`latlng`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+以下メモ
+CREATE TABLE `dcard_db`.`csv_tbl`(
+    `paymant_month` INT,
+    `name` VARCHAR(10),
+    `date` DATE,
+    `description` VARCHAR(45),
+    `amount` INT,
+    `amount2` INT,
+    `note` VARCHAR(45)
+);
