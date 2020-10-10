793046643d42a265a28dafdd53267866029855f0
k-furumichi <k-furumichi@sakura.ad.jp>
2020-10-10 19:20:49 +0900

    誤記修正

--- db/initdb.d/create_tbl.sql
+++ db/initdb.d/create_tbl.sql
@@ -3,7 +3,7 @@ CREATE TABLE `travel_map_db`.`travel_map_tbl`(
     `latlng` geometry NOT NULL,
     `date` DATE,
     `destination` VARCHAR(40),
-    `category` VARCHAR(20)
+    `category` VARCHAR(20),
     `spot_name` VARCHAR(40),
     `note` VARCHAR(40),
     `img_path` VARCHAR(128),
