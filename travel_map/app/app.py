adbf5a9924be79c76811a2c1e8130b29f2a2f29e
k-furumichi <k-furumichi@sakura.ad.jp>
2020-10-14 21:31:19 +0900

    修正

--- travel_map/app/app.py
+++ travel_map/app/app.py
@@ -16,7 +16,7 @@ def entry_get():
 @app.route("/entry", methods=['POST'])
 def entry_post():
     entry_post_value = request.form
-    with open('/var/www/html/app/test.txt', mode="w") as f:
+    with open('/var/www/html/app/test.txt', mode="w", encoding='shift_jis') as f:
         for key,value in entry_post_value.items():
             f.write(str(type(entry_post_value)))
             f.write(f'{key}:{value}\n')
