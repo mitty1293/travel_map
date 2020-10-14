123c3c0ba74cf89cf3d81f8e24b432ad615f4c7c
k-furumichi <k-furumichi@sakura.ad.jp>
2020-10-14 18:36:47 +0900

    ルーティング追加

--- travel_map/app/app.py
+++ travel_map/app/app.py
@@ -5,20 +5,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/", methods=['GET'])
+def index_get():
     return render_template("index.html")
 
-@app.route("/entry")
-def entry():
+@app.route("/entry", methods=['GET'])
+def entry_get():
     return render_template("entry.html")
 
-@app.route("/edit")
-def edit():
+@app.route("/entry", methods=['POST'])
+def entry_post():
+    return render_template("entry.html")
+
+@app.route("/edit", methods=['GET'])
+def edit_get():
     return render_template("edit.html")
 
-@app.route("/delete")
-def delete():
+@app.route("/edit", methods=['POST'])
+def edit_post():
+    return render_template("edit.html")
+
+@app.route("/delete", methods=['GET'])
+def delete_get():
+    return render_template("delete.html")
+
+@app.route("/delete", methods=['POST'])
+def delete_post():
     return render_template("delete.html")
 
 if __name__ == '__main__':
