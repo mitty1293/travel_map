b78c9f7bf740b905a83b8bc8caf34aa1128fb859
mitty1293 <k.fmichi@gmail.com>
2020-10-21 12:44:20 +0900

    スクリプトを外部ファイルに

---
+++ travel_map/app/templates/map.js
@@ -0,0 +1,7 @@
+function initMap(){
+    var opts = {
+        zoom: 15,
+        center: new google.maps.LatLng(35.709984,139.810703)
+    };
+    var map = new google.maps.Map(document.getElementById("map"), opts);
+}
