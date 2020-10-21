2388c79f04e91481549c2dd8b58dcf5c4f88d89d
mitty1293 <k.fmichi@gmail.com>
2020-10-21 13:20:53 +0900

    クリック時のマーカー生成

--- travel_map/app/static/js/map.js
+++ travel_map/app/static/js/map.js
@@ -4,4 +4,13 @@ function initMap(){
         center: new google.maps.LatLng(35.709984,139.810703)
     };
     var map = new google.maps.Map(document.getElementById("map"), opts);
+
+    map.addListener('click', function(e){
+        var marker = new google.maps.Marker({
+            position: e.latLng,
+            map: map,
+            title: e.latLng.toString(),
+            animation: google.maps.Animation.DROP
+        });
+    });
 }
