cba84341348488d8de7f7f6738811fc7e4b2f6a2
k-furumichi <k-furumichi@sakura.ad.jp>
2020-10-21 18:59:10 +0900

    修正

--- travel_map/app/static/js/gmap.js
+++ travel_map/app/static/js/gmap.js
@@ -1,9 +1,10 @@
+var map;
 function initMap(){
     var opts = {
         zoom: 15,
         center: new google.maps.LatLng(35.709984,139.810703)
     };
-    var map = new google.maps.Map(document.getElementById("map"), opts);
+    map = new google.maps.Map(document.getElementById("gmap"), opts);
 
     map.addListener('click', function(e){
         var marker = new google.maps.Marker({
