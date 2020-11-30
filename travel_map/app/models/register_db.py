0a2090f90833f4665d6e845689c944cc5b9ba15b
k-furumichi <k-furumichi@sakura.ad.jp>
2020-11-30 15:39:46 +0900

    修正

--- travel_map/app/models/register_db.py
+++ travel_map/app/models/register_db.py
@@ -13,7 +13,7 @@ def register_db(**data):
     data['lat'] = 35.713364036866
     data['lng'] = 139.80851431744384
     with open('/var/www/html/app/test2.txt', mode="w", encoding='shift_jis') as f:
-        f.write(str(type(data))+'\n')
+        f.write(f'{str(type(data))}\n')
         for key,value in data.items():
             f.write(str(type(value)))
             f.write(f'{key}:{value}\n')
