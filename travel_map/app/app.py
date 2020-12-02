94b6d31bfc7b78ec46e61e673c7e41cc0f0ad924
mitty1293 <k.fmichi@gmail.com>
2020-12-02 22:26:57 +0900

    必要モジュールimport

--- travel_map/app/app.py
+++ travel_map/app/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # _*_ coding: utf-8 _*_
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from models import register_db
 import random, string
 
