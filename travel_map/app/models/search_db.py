2164b0ebadddb5a5500a9c6684c7dcd812699c40
mitty1293 <k.fmichi@gmail.com>
2020-12-05 19:14:17 +0900

    検索機能の検討

--- travel_map/app/models/search_db.py
+++ travel_map/app/models/search_db.py
@@ -6,3 +6,5 @@ import mysql.connector
 def search_db(**data):
     connect = mysql.connector.connect(host="travel_map_db", port="3306", user="user", password="password", database="travel_map_db")
     cursor = connect.cursor()
+
+    SEARCH_SQL = 'SELECT'
