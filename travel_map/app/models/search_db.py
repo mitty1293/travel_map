595ff0515d97944ce90863b3429c9ba1e6e462bd
k-furumichi <k-furumichi@sakura.ad.jp>
2020-12-06 23:54:43 +0900

    デバッグ用処理追加

--- travel_map/app/models/search_db.py
+++ travel_map/app/models/search_db.py
@@ -7,6 +7,16 @@ def search_db(**data):
     connect = mysql.connector.connect(host="travel_map_db", port="3306", user="user", password="password", database="travel_map_db")
     cursor = connect.cursor()
 
+    # debug用
+    with open('/var/www/html/app/search_db_test.txt', mode="w", encoding='shift_jis') as f:
+        f.write(str(type(data)))
+        f.write('\n')
+        for key,value in data.items():
+            f.write(str(type(value)))
+            f.write(f'{key}:{value}\n')
+        f.write(",".join(map(str,list(data.values()))))
+    # ここまで
+
     SEARCH_SQL = 'SELECT * FROM travel_map_tbl WHERE category=%s'
     cursor.execute(SEARCH_SQL, (data['category'], ))
     return cursor.fetchall()
