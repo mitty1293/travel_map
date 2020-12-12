3caa9074df7a06cbc07ec1946a0b99a33188e9c3
k-furumichi <k-furumichi@sakura.ad.jp>
2020-12-13 06:24:01 +0900

    latとlngを分けた

--- db/initdb.d/create_tbl.sql
+++ db/initdb.d/create_tbl.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `travel_map_db`.`travel_map_tbl`(
     `id` bigint(16) unsigned NOT NULL AUTO_INCREMENT,
-    `latlng` geometry NOT NULL,
+    `lat` DOUBLE(9,6),
+    `lng` DOUBLE(9,6),
     `date` DATE,
     `destination` VARCHAR(40),
     `category` VARCHAR(20),
