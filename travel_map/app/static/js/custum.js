c89169834ce5f2ff5337b237c748af52351ab36e
mitty1293 <k.fmichi@gmail.com>
2020-12-20 09:03:46 +0900

    fix

---
+++ travel_map/app/static/js/custum.js
@@ -0,0 +1,8 @@
+function addCategory() {
+    var select = document.getElementById("categoryselect");
+    var option = document.createElement("option");
+    var added_category = document.getElementById("added_category").value;
+    option.text = added_category;
+    option.value = added_category;
+    select.appendChild(option);
+}
