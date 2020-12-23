6fa96405d4043df139d1f25b7f11e5e1cb28eaf2
mitty1293 <k.fmichi@gmail.com>
2020-12-23 15:43:41 +0900

    fix

--- travel_map/app/static/js/custum.js
+++ travel_map/app/static/js/custum.js
@@ -5,5 +5,5 @@ function addCategory() {
     option.text = added_category;
     option.value = added_category;
     select.appendChild(option);
-    $('#msg').html('<div class="alert alert-success fade in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button>This is a success message</div>');
+    select.value = added_category;
 }
